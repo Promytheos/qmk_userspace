71d26cf39c98457b0dfe38830c0e0038b60ad83f
Luke Swan <swan.luke9@gmail.com>
2026-03-16 03:32:18 +0200

    add license to satisfy linter

--- keyboards/splitkb/halcyon/corne/keymaps/vortex_hlc/config.h
+++ keyboards/splitkb/halcyon/corne/keymaps/vortex_hlc/config.h
@@ -1,3 +1,6 @@
+// Copyright 2026 phage-zn
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #define CAPS_WORD_INVERT_ON_SHIFT
 #define TAPPING_TERM 175
 #define TAPPING_TERM_PER_KEY
--- keyboards/splitkb/halcyon/corne/keymaps/vortex_hlc/keymap.c
+++ keyboards/splitkb/halcyon/corne/keymaps/vortex_hlc/keymap.c
@@ -1,3 +1,6 @@
+// Copyright 2026 phage-zn
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include QMK_KEYBOARD_H
 
 enum layers {
